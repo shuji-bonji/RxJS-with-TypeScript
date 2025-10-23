7e847ef1c74ebcdcd4a25222549ee3c25d1b6003
shuji-bonji <shuji-bonji@users.noreply.github.com>
2025-10-23 14:08:49 +0000

    deploy: 2300dc56bb8a26d821e65ef687d43bf6cbc8acfb

---
+++ assets/chunks/stateDiagram-v2-YXO3MK2T.DHGwKKlN.js
@@ -0,0 +1,1 @@
+import{s as e,b as r,a,S as s}from"./chunk-AEK57VVT.NYUeQhbc.js";import{_ as i}from"../app.D0WEYNts.js";import"./chunk-RZ5BOZE2.D-lZwIZg.js";import"./framework.BvXV5w7a.js";import"./theme.nLYA0Pla.js";var p={parser:a,get db(){return new s(2)},renderer:r,styles:e,init:i(t=>{t.state||(t.state={}),t.state.arrowMarkerAbsolute=t.arrowMarkerAbsolute},"init")};export{p as diagram};
